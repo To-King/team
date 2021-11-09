e23ee6c43b7a3c63ce26d8398059378f4c475490
ugg999999 <ugg999999@gmail.com>
2021-11-09 11:01:18 +0800

    Initial commit

--- jd_cfd.js
+++ jd_cfd.js
@@ -88,7 +88,7 @@ if ($.isNode()) {
   if (!res) {
     $.http.get({url: 'https://purge.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/cfd.json'}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
     await $.wait(1000)
-    res = await getAuthorShareCode('https://raw.githubusercontent.com/ugg999999/Profiles/master/shareCodes/cfd.json')
+    res = await getAuthorShareCode('https://raw.fastgit.org/ugg999999/Profiles/master/shareCodes/cfd.json')
   }
   $.strMyShareIds = [...(res && res.shareId || [])]
   await shareCodesFormat()
